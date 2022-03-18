37b9fc77955e9ab6add607fbf2fe76bb49bbf009
snowyru <97807830+snowyru@users.noreply.github.com>
2022-03-18 16:32:41 +0200

    indented code and added comments

--- capture-beauty/src/Login.js
+++ capture-beauty/src/Login.js
@@ -18,6 +18,7 @@ function Login() {
 
     // ThemeProvider for later
     const theme = createTheme();
+        //currently primary is blue, secondary is purple
 
     const handleLogin = (event) => {
         event.preventDefault();
@@ -43,62 +44,67 @@ function Login() {
                         flexDirection: 'column',
                         alignItems: 'center',
                     }}>
-
-                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        {/* Circled lock icon */}
+                        <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
                             <LockOutlinedIcon />
                         </Avatar>
-                        <Typography component="h1" variant="h5">
-                            Log in
-                        </Typography>
-                        <Box component="form" onSubmit={handleLogin} noValidate sx={{ mt: 1 }}>
-                            <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                            />
-                            <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                            />
-                            <FormControlLabel
-                            control={<Checkbox value="remember" color="primary" />}
-                            label="Remember me"
-                            />
-                            <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                            >
-                            Log In
-                            </Button>
-                            <Grid container>
-                            <Grid item xs>
-                                <Link href="#" variant="body2">
-                                Forgot password?
-                                </Link>
-                            </Grid>
-                            <Grid item>
-                                <Link href="#" variant="body2">
-                                {"Don't have an account? Register now!"}
-                                </Link>
-                            </Grid>
-                            </Grid>
-                        </Box>
+                            {/* Log in title */}
+                            <Typography component="h1" variant="h5">
+                                Log in
+                            </Typography>
+                                {/* Text box for email input */}
+                                <Box component="form" onSubmit={handleLogin} noValidate sx={{ mt: 1 }}>
+                                    <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                    autoFocus
+                                    />
+                                        {/* Text box for password input */}
+                                        <TextField
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="Password"
+                                        type="password"
+                                        id="password"
+                                        autoComplete="current-password"
+                                        />
+                                            {/* Remembers jwt token */}
+                                            <FormControlLabel
+                                            control={<Checkbox value="remember" color="primary" />}
+                                            label="Remember me"
+                                            />
+                                                {/* Log in button */}
+                                                <Button
+                                                type="submit"
+                                                fullWidth
+                                                variant="contained"
+                                                sx={{ mt: 3, mb: 2 }}>
+                                                Log In
+                                                </Button>
+                                                    {/* Forgot password or register links */}
+                                                    <Grid container>
+                                                        <Grid item xs>
+                                                            <Link href="#" variant="body2">
+                                                            Forgot password?
+                                                            </Link>
+                                                        </Grid>
+                                                            <Grid item>
+                                                                <Link href="#" variant="body2">
+                                                                {"Don't have an account? Register now!"}
+                                                                </Link>
+                                                            </Grid>
+                                                    </Grid>
+                                </Box>
                     </Box>
                 </Container>
-                </ThemeProvider>
+            </ThemeProvider>
    
             {/* End of login page */}
         <Footer/>
