31b67cec8ac852955d0b64528d3a939cad1cdb44
snowyru <97807830+snowyru@users.noreply.github.com>
2022-03-18 14:25:17 +0200

    install MUI copied template with Nav/foot

--- capture-beauty/src/App.js
+++ capture-beauty/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { BrowserRouter, Switch } from 'react-router-dom';
 import LayoutRoute from './LayoutRoute';
+import Album from './Login';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <LayoutRoute>
-          
+          <LayoutRoute path="/" component={Album} exact="true" />
         </LayoutRoute>
       </Switch>
     </BrowserRouter>
