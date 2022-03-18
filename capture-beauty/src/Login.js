01e186a6998616752a6d075aee0cbb0166016774
snowyru <97807830+snowyru@users.noreply.github.com>
2022-03-18 16:22:55 +0200

    Added signup/login template (w)

--- capture-beauty/src/Login.js
+++ capture-beauty/src/Login.js
@@ -1,34 +1,105 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
-import CameraIcon from '@mui/icons-material/PhotoCamera';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import NavBar from './NavBar';
 import Footer from './Footer';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function Login() {
 
+    // ThemeProvider for later
+    const theme = createTheme();
 
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+          email: data.get('email'),
+          password: data.get('password'),
+        });
+      };
 
     return(
         <>
         <NavBar/>
             {/* Start of login page */}
+                {/* Empty theme provider for later  */}
+               <ThemeProvider theme={theme}> 
+                <Container component="main" maxWidth="xs">
+                    {/* Box of whole log in component */}
+                    <Box
+                    sx={{
+                        marginTop: 7,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}>
 
-        <h1>Testing123</h1>
-        
+                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Log in
+                        </Typography>
+                        <Box component="form" onSubmit={handleLogin} noValidate sx={{ mt: 1 }}>
+                            <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                            />
+                            <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            />
+                            <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                            />
+                            <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                            >
+                            Log In
+                            </Button>
+                            <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                {"Don't have an account? Register now!"}
+                                </Link>
+                            </Grid>
+                            </Grid>
+                        </Box>
+                    </Box>
+                </Container>
+                </ThemeProvider>
+   
             {/* End of login page */}
         <Footer/>
         </>
